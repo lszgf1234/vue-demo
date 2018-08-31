62b4ebef05ea2488b877f24a0b43c8d14a4905f3
Li Shuai <1076283698@qq.com>
2018-08-31 18:10:28 +0800

    sass styl编译

---
+++ src/views/element.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="jj-element">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+  import ElementUI from 'element-ui';
+  export default {
+    name: 'element',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+        dataList: [
+          {}
+        ]
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
